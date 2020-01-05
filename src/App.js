b32f32fbc4a22716f078b08f5d9add79a4a87cd9
Christian Ramírez <christiandr@titamedia.com>
2020-01-05 11:00:15 -0500

    Primer commit en react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" src="https://i.pinimg.com/originals/09/eb/2a/09eb2a0df057172facf31015a65382f8.jpg" alt="city"/>
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Cursos de EDteam</p>
+            <p>Tu futuro te está esperando</p>
+            <a href="https://ed.team" className="button">Suscribirse</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
+    <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+      <Curso />
+    </div>
+  </>
+)
 
 export default App;
+
+
+// Reglas JSX
+// 1. Toda etiqueta debe cerrarse
+// 2. Los componentes deben devolver un solo elemento padre
+// 3. Apoyarse de los Fragment(<> </>) cuando necesito devolver dos elementos
+// 4. Fragment  =>  <> hijos </>
+// 5. img siempre se cierra
+// 6. className => classNameName
+// 7. for => htmlFor
+// 8. NO if, else, while
